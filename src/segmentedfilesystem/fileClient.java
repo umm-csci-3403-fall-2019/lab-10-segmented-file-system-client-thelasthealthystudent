130912b1438b246fa75909cf337fd1c926f7c546
drumm040 <mdrumm02>
2019-12-12 22:06:06 -0600

    Does not print correct output, but I am still very close

--- src/segmentedfilesystem/fileClient.java
+++ src/segmentedfilesystem/fileClient.java
@@ -10,6 +10,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.lang.Math.*;
 import java.lang.reflect.Array;
+import java.util.Collections;
+import java.util.Comparator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class fileClient {
 
@@ -21,9 +27,6 @@ public class fileClient {
 
 
     public static void main(String[] args) throws IOException {
-        OutputStream OStream1 = new FileOutputStream("file1");
-        OutputStream OStream2 = new FileOutputStream("file2");
-        OutputStream OStream3 = new FileOutputStream("file3");
         DatagramSocket socket = new DatagramSocket();
         byte[] buf = new byte[1028];
         InetAddress address = InetAddress.getByName(stringAddress);
@@ -33,10 +36,12 @@ public class fileClient {
 
         packet = new DatagramPacket(buf, buf.length);
         socket.receive(packet);
-        String received = new String(packet.getData(), 0, packet.getLength());
+        String received = new String(packet.getData(), 4, packet.getLength()-4);
         System.out.println("Pointless String should print: " + received);
         int i = 1;
-        
+
+        FileOutputStream fos = new FileOutputStream("file2.txt");
+
         file file1 = new file();
         file file2 = new file();
         file file3 = new file();
@@ -44,66 +49,77 @@ public class fileClient {
         while (!(file1.fileDone && file2.fileDone && file3.fileDone)){
             socket.receive(packet);
             byte[] rawData = packet.getData();
-            byte[] status = Arrays.copyOfRange(rawData, 0, 1);
+            int status = ((byte)Array.get(rawData, 0));
             //byte[] fileID = Arrays.copyOfRange(rawData, 1,2);
-            int fileID = ((byte)Array.get(rawData, 1));
+            int fileID = rawData[1];
+            int the = packet.getData()[1];
             byte[] packetNumber = Arrays.copyOfRange(rawData, 2,4);
-            byte[] data = Arrays.copyOfRange(rawData, 4, packet.getLength());
+            byte[] data = Arrays.copyOfRange(rawData, 4, 1028);
             ByteBuffer wrapped = ByteBuffer.wrap(packetNumber);
 
-            if (file1.noFileID() && !(fileID == file1.fileID)) {
+            //find the IDs of our three files so we can reassemble them later
+            if (file1.noFileID() && !(fileID == file2.fileID) && !(fileID == file3.fileID)) {
                 file1.fileID = fileID;
-                System.out.println("File1 is: " + fileID);
-            } else if (file2.noFileID() && !(fileID == file2.fileID)) {
+                System.out.println("FileID1 is: " + fileID);
+            } else if (file2.noFileID() && !(fileID == file1.fileID) && !(fileID == file3.fileID)){
                 file2.fileID = fileID;
-                System.out.println("File2 is: " + fileID);
+                System.out.println("FileID2 is: " + fileID);
             } else if (file3.noFileID() && !(fileID == file1.fileID) && !(fileID == file2.fileID)) {
                 file3.fileID = fileID;
-                System.out.println("File3 is: " + fileID);
+                System.out.println("FileID3 is: " + fileID);
             }
             
 
             //1st bit is x, second is y 000000yx
-            if ( (status[0] % 2) == 0 ) {
+            if ( (status % 2) == 0 ) {
                 //if even, then its a header
                 data = Arrays.copyOfRange(rawData, 2, packet.getLength());
                 if (fileID == file1.fileID) {
                     file1.fileName = new String(data);
-                    System.out.println(file1.fileName + "1packet number: " + wrapped.getShort());
+                    System.out.println("File1 Name: " + file1.fileName);
+                    //System.out.println("packet number: " + wrapped.getShort());
                 } else if (fileID == file2.fileID ) {
-                    file2.fileName= Arrays.toString(data);
-                    System.out.println(file2.fileName + "2packet number: " + wrapped.getShort());
+                    file2.fileName= new String(data);
+                    System.out.println("File2 Name: " + file2.fileName);
+                    //System.out.println("packet number: " + wrapped.getShort());
                 } else if (fileID == file3.fileID) {
-                    file3.fileName = Arrays.toString(data);
-                    System.out.println(file3.fileName + "3packet number: " + wrapped.getShort());}
+                    file3.fileName = new String(data);
+                    System.out.println("File3 Name: " + file3.fileName);
+                    //System.out.println("packet number: " + wrapped.getShort());
+                }
 
-            } else if ((status[0] % 4) == 3) {
+            } else if ((status % 4) == 3) {
                 //if 3 mod 4, then this is the last data packet for this file
                 int num = wrapped.getShort();
 
                 if (fileID == file1.fileID) {
                     file1.numPackets = num;
+                    file1.fileArray.add(rawData);
                     System.out.println("1tail packet number: " + file1.numPackets);
                 } else if (fileID == file2.fileID) {
                     file2.numPackets = num;
+                    file2.fileArray.add(rawData);
                     System.out.println("2tail packet number: " + file2.numPackets);
                 } else if (fileID == file3.fileID) {
                     file3.numPackets = num;
+                    file3.fileArray.add(rawData);
                     System.out.println("3tail packet number: " + file3.numPackets);
                 }
-            } else {
+            } else if ((status % 2) == 1) {
                 //if we get here, that means this is a regular data packet.
                 if (fileID == file1.fileID) {
-                    file1.fileArray.add(data);
+                    file1.fileArray.add(rawData);
                 } else if (fileID == file2.fileID) {
-                    file2.fileArray.add(data);
+                    file2.fileArray.add(rawData);
+                    fos.write(rawData);
                 } else if (fileID == file3.fileID) {
-                    file3.fileArray.add(data);
-                    //OStream3.write(data);
+                    file3.fileArray.add(rawData);
                 }
+            } else {
+                System.out.println("THIS SHOULD NEVER RUN");
             }
 
-            System.out.println("Packet: " + i);
+            System.out.println(i);
             i++;
 
 
@@ -120,22 +136,17 @@ public class fileClient {
                 System.out.println("file3 is done.");
             }
         }
+        System.out.println("Done receiving packets.");
+
+        file1.sortFileArray();
+        file2.sortFileArray();
+        file3.sortFileArray();
+        file1.writeFile();
+        file2.writeFile();
+        file3.writeFile();
+
         System.out.println("Done writing.");
 
-        sortBytes();
-
-
-        for (i = 0; i < file1.fileArray.size() ; i++) {
-
-        }
-    }
-
-    public static void sortBytes() {
-
-    }
-
-    public boolean compareBytes(byte[] A1, byte[] A2) {
-        return A1[1] > A2[1];
     }
 
     public static class file{
@@ -191,8 +202,76 @@ public class fileClient {
         public boolean noFileID() {
             return (this.fileID == Integer.MAX_VALUE);
         }
-        
+
+        public void sortFileArray() {
+
+//            int numSwitches = 1;
+//            //I had troubles using a comparator class,
+//            //so this sorting method uses numSwitches to determine if our last pass through changed any elements.
+//            //hence if this is zero by the end of a loop, the array was sorted.
+//            while (!(numSwitches == 0)) {
+//                numSwitches = 0;
+//            for (int i = 0; i < (this.fileArray.size() - 1) ; i++) {
+//                ByteBuffer wrapped1 = ByteBuffer.wrap(Arrays.copyOfRange(this.fileArray.get(i), 2,4));
+//                ByteBuffer wrapped2 = ByteBuffer.wrap(Arrays.copyOfRange(this.fileArray.get(i+1), 2,4));
+//                if( wrapped1.getShort() > wrapped2.getShort()) {
+//                    byte[] tempvar = (this.fileArray.get(i)).clone();
+//                    this.fileArray.set(i, (this.fileArray.get(i + 1)).clone());
+//                    this.fileArray.set(i + 1, tempvar);
+//                    numSwitches++;
+//                }
+//                System.out.println(Arrays.toString(this.fileArray.get(i)));
+//                }
+//            }
+            this.fileArray.sort(new sortByPacket());
+            System.out.println("Done sorting: " + this.fileName);
+        }
+
+        public void writeFile() throws IOException {
+
+            File f = new File(this.fileName);
+
+            FileOutputStream FOStream = new FileOutputStream(f);
+
+            try {
+
+            for (int j = 0; j <= this.fileArray.size(); j++){
+                FOStream.write(Arrays.copyOfRange(this.fileArray.get(j), 4, this.fileArray.get(j).length));
+            }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found" + e);
+            } catch (IOException ioe) {
+                System.out.println("Exception while writing file " + ioe);
+            }
+            finally {
+                // close the streams using close method
+                try {
+                    if (FOStream != null) {
+                    }
+                    FOStream.close();
+                } catch (IOException ioe) {
+                    System.out.println("Error while closing stream: " + ioe);
+                }
+            }
+
+        }
+
     }
+
+    static class sortByPacket implements Comparator<byte[]> {
+        public int compare(byte[] b1, byte[] b2) {
+            ByteBuffer wrapped1 = ByteBuffer.wrap(Arrays.copyOfRange(b1, 2,4));
+            ByteBuffer wrapped2 = ByteBuffer.wrap(Arrays.copyOfRange(b2, 2,4));
+            return Integer.compare(wrapped1.getShort(), wrapped2.getShort());
+        }
+    }
+
+//Below are old methods I longer need, but might want to refer to for help later perhaps.
+
+//    public boolean compareBytes(byte[] A1, byte[] A2) {
+//        return A1[1] > A2[1];
+//    }
+
 
 //    public class byteSort {
 //        private byte[] data;
